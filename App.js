ba560073fb162d83a5eb941d9bf122d21f101d23
darren lackie <lackiewwpj@gmail.com>
2023-11-25 15:48:21 +0000

    front end talking to server. Displaying bike spots using long, lat from database

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import Map from './components/Map/Map';
-
+import { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
 
 export default function App() {
-  return (
 
-      <Map />
+  const [bicyclespots, setBicycleSpots] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:8080/bicyclespots')
+    .then(res => res.json())
+    .then(spotData => setBicycleSpots(spotData))
+  }, [])
+
+  const bicycleItems = bicyclespots.map((spot) =>
+    spot.latitude
+  )
+
+  return (
+    // <View>
+    //     <Text>{bicycleItems.join(', ')}</Text>
+    // </View>
+    
+    <Map />
 
   );
 }
--- components/Map/Map.js
+++ components/Map/Map.js
@@ -1,19 +1,39 @@
-import { View } from 'react-native'
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native'; // Make sure to import Text from react-native
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import styles from './styles';
 
 const Map = () => {
-    return ( 
+    const [bicyclespots, setBicycleSpots] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/bicyclespots')
+            .then(res => res.json())
+            .then(spotData => setBicycleSpots(spotData))
+    }, []);
+
+    const bicycleItems = bicyclespots.map((spot, index) => (
+        <Marker
+            key={spot.council_identifier}
+            coordinate={{ latitude: spot.latitude, longitude: spot.longitude }}
+            title={`Capacity: ${spot.capacity}`}
+            pinColor='blue'
+        />
+    ));
+
+// ToDo: We can set showUserLocation on MapView and we need to look into react-native-permissions
+    return (
         <View style={styles.container}>
-            <MapView 
-            style={styles.map} 
-            provider={PROVIDER_GOOGLE}                 
-            region={{
-                latitude: 55.9533,
-                longitude: -3.1883,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421
-            }}>
+            <MapView
+                style={styles.map}
+                provider={PROVIDER_GOOGLE}
+                region={{
+                    latitude: 55.9533,
+                    longitude: -3.1883,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421
+                }}>
+                {bicycleItems}
             </MapView>
         </View>
     );
