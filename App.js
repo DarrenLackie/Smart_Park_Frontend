d1a5b12c69212a30b0cae22e014fb3a7a38f1731
darren lackie <lackiewwpj@gmail.com>
2023-11-25 17:38:14 +0000

    added clustering, not perfect yet. Needs work

--- App.js
+++ App.js
@@ -4,18 +4,6 @@ import { View, Text } from 'react-native';
 
 export default function App() {
 
-  const [bicyclespots, setBicycleSpots] = useState([])
-
-  useEffect(() => {
-    fetch('http://localhost:8080/bicyclespots')
-    .then(res => res.json())
-    .then(spotData => setBicycleSpots(spotData))
-  }, [])
-
-  const bicycleItems = bicyclespots.map((spot) =>
-    spot.latitude
-  )
-
   return (
     // <View>
     //     <Text>{bicycleItems.join(', ')}</Text>
--- components/Map/Map.js
+++ components/Map/Map.js
@@ -1,25 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native'; // Make sure to import Text from react-native
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import { View, Text } from 'react-native';
+import { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import styles from './styles';
+import MapView from 'react-native-map-clustering';
 
 const Map = () => {
-    const [bicyclespots, setBicycleSpots] = useState([]);
+
+    const initialRegion = {
+        latitude: 55.9533,
+        longitude: -3.1883,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+    }
+
+    const [bicyclespots, setBicycleSpots] = useState([])
 
     useEffect(() => {
         fetch('http://localhost:8080/bicyclespots')
             .then(res => res.json())
             .then(spotData => setBicycleSpots(spotData))
-    }, []);
+    }, [])
 
-    const bicycleItems = bicyclespots.map((spot, index) => (
+    const bicycleItems = bicyclespots.map((spot) => (
         <Marker
             key={spot.council_identifier}
             coordinate={{ latitude: spot.latitude, longitude: spot.longitude }}
             title={`Capacity: ${spot.capacity}`}
-            pinColor='blue'
         />
-    ));
+    ))
 
 // ToDo: We can set showUserLocation on MapView and we need to look into react-native-permissions
     return (
@@ -27,16 +35,14 @@ const Map = () => {
             <MapView
                 style={styles.map}
                 provider={PROVIDER_GOOGLE}
-                region={{
-                    latitude: 55.9533,
-                    longitude: -3.1883,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421
-                }}>
+                minPoints={2}
+                region={initialRegion}
+                radius={200}
+                >
                 {bicycleItems}
             </MapView>
         </View>
-    );
+    )
 }
 
 export default Map;
