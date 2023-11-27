7279da447006b4c09deed4ecaf3968760124513d
darren lackie <lackiewwpj@gmail.com>
2023-11-27 18:57:23 +0000

    autocomplete now works, no functionality for address yet

--- components/SearchBar/LocationSearch.js
+++ components/SearchBar/LocationSearch.js
@@ -1,54 +1,46 @@
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
-import APIKEY from '../../secrets'  
-// import { StyleSheet } from "react-native";
+import APIKEY from '../../secrets'
 
-// const styles = StyleSheet.create({
-//     container: {
-//         padding: 10,
-//         height:'100%'
-//     },
-//     textInput: {
-//         marginLeft: 10,
-//         marginRight: 10,
-//         marginTop: 50,
-//         height: 50,
-//         color: '#5d5d5d',
-//         fontSize: 16,
-//     },
-// })
+    const LocationSearch = () => {
 
-const LocationSearch = () => {
-    return (
-        <GooglePlacesAutocomplete
+        const handlePlaceSelection = (data, details = null) => {
+            const { lat, lng } = details.geometry.location
+            console.log('Selected location:', { latitude: lat, longitude: lng })
+        }
+
+        return (
+            <GooglePlacesAutocomplete
                 placeholder="Search for a location"
-                onPress={(data, details = null) => {
-                    console.log(data, details)
-                }}
+                debounce={100}
+                minLength={2}
                 query={{
                     key: APIKEY,
                     language: 'en',
                 }}
+                onPress={handlePlaceSelection}
                 fetchDetails={true}
+                returnKeyType={"search"}
+                enablePoweredByContainer={false}
                 styles={{
                     textInputContainer: {
                         backgroundColor: 'rgba(0,0,0,0)',
                         borderTopWidth: 0,
                         borderBottomWidth: 0,
                     },
-                textInput: {
-                    marginLeft: 10,
-                    marginRight: 10,
-                    marginTop: 50,
-                    height: 50,
-                    color: '#5d5d5d',
-                    fontSize: 16,
-                },
-                predefinedPlacesDescription: {
-                    color: '#1faadb',
-                }
-            }}
-        />
-    )
-}
+                    textInput: {
+                        marginLeft: 10,
+                        marginRight: 10,
+                        marginTop: 50,
+                        height: 50,
+                        color: '#5d5d5d',
+                        fontSize: 16,
+                    },
+                    predefinedPlacesDescription: {
+                        color: '#1faadb',
+                    }
+                }}
+            />
+        )
+    }
 
 export default LocationSearch
