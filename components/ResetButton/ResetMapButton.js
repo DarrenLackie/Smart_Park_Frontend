7dbc9b3f8478651cdbaee60fd9b5da6083192c55
darren lackie <lackiewwpj@gmail.com>
2023-11-28 14:38:49 +0000

    reset button feature now works

---
+++ components/ResetButton/ResetMapButton.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+const ResetMapButton = ({ onResetMap }) => {
+    
+    const handleResetMap = () => {
+        onResetMap();
+    };
+
+    return (
+        <View style={styles.button}>
+            <Button 
+            title="Reset Map" 
+            onPress={handleResetMap} 
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    button: {
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 200,
+        height: 100,
+        color: '#5d5d5d',
+        position: 'absolute'
+    }
+})
+
+export default ResetMapButton;
